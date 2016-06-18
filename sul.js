609cd99d5e71b41f5ca60f42ec53b015f7bc71ed
Nikita Kogut <mronlinecoder@gmail.com>
2016-06-18 17:57:43 +0300

    Added IIFE wrapper

--- sul.js
+++ sul.js
@@ -7,58 +7,81 @@
 
 'use strict';
 
-function SUL_Object(domElement) {
-	this.native = domElement;
-}
+(function () {
 
-SUL_Object.prototype.html = function(val) {
-	if (val == undefined || val == null) {
-		return this.native.innerHTML;
+	//#########
+	//Global Part
+	//#########
+
+	function sulNoop() {
+		// nope :)
 	}
 
-	this.native.innerHTML = val;
-	return this;
-};
+	//#########
+	//DOM Part
+	//#########
 
-SUL_Object.prototype.val = function(newval) {
-	if (newval == undefined || newval == null) {
-		return this.native.value;
+	function SUL_Object(domElement) {
+		this.native = domElement;
 	}
 
-	this.native.value = newval;
-	return this;
-};
+	SUL_Object.prototype.html = function(val) {
+		if (val == undefined || val == null) {
+			return this.native.innerHTML;
+		}
+
+		this.native.innerHTML = val;
+		return this;
+	};
+
+	SUL_Object.prototype.val = function(newval) {
+		if (newval == undefined || newval == null) {
+			return this.native.value;
+		}
+
+		this.native.value = newval;
+		return this;
+	};
 
 
-SUL_Object.prototype.hide = function() {
-	this.native.style.display = "none";
-	return this;
-};
+	SUL_Object.prototype.hide = function() {
+		this.native.style.display = "none";
+		return this;
+	};
 
-SUL_Object.prototype.show = function() {
-	this.native.style.display = "block";
-	return this;
-};
+	SUL_Object.prototype.show = function() {
+		this.native.style.display = "block";
+		return this;
+	};
 
-SUL_Object.prototype.hasClass = function(c) {
-	return this.native.className.indexOf(c) > -1;
-};
+	SUL_Object.prototype.hasClass = function(c) {
+		return this.native.className.indexOf(c) > -1;
+	};
 
-SUL_Object.prototype.addClass = function(c) {
-	this.native.className += (" "+c); 
-	return this;
-};
+	SUL_Object.prototype.addClass = function(c) {
+		this.native.className += (" "+c); 
+		return this;
+	};
 
-SUL_Object.prototype.removeClass = function(c) {
-	this.native.className = this.native.className.replace(c, "").trim();
-	return this;
-};
+	SUL_Object.prototype.removeClass = function(c) {
+		this.native.className = this.native.className.replace(c, "").trim();
+		return this;
+	};
 
-SUL_Object.prototype.on = function(event, handler) {
-	this.native.addEventListener(event, handler);
-	return this;
-};
+	SUL_Object.prototype.on = function(event, handler) {
+		this.native.addEventListener(event, handler);
+		return this;
+	};
 
-function SUL(selector) {
-	return new SUL_Object(document.querySelector(selector));
-};
+	function SUL(selector) {
+		return new SUL_Object(document.querySelector(selector));
+	};
+
+
+	//#########
+	//End
+	//#########
+	
+	window.SUL = SUL;
+	window.SULX = SULX_Object;
+}());
